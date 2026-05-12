695d2f0b9537f15da0d5400a30c5ab68bd24ade1
cwossant <dwayynee@gmail.com>
2026-05-13 07:54:46 +0800

    Refactor Customer QR Scan and Scanner Panels
    - Updated CustomerQrScanPanel and CustomerQrScannerPanel to improve button navigation and visibility.
    - Added action listeners for buttons to navigate to respective panels.
    - Made buttons invisible but still clickable to enhance UI experience.
    - Adjusted layout to use AbsoluteLayout for better control over component positioning.
    - Removed unnecessary components and cleaned up the form files.

--- src/main/java/com/mycompany/barkbites/CustomerForms/CustomerProfilePanelVisible.java
+++ src/main/java/com/mycompany/barkbites/CustomerForms/CustomerProfilePanelVisible.java
@@ -5,7 +5,39 @@ public class CustomerProfilePanelVisible extends javax.swing.JFrame {
     public CustomerProfilePanelVisible() {
         initComponents();
 
+        // Ensure the background image doesn't sit on top of the click targets.
+        getContentPane().setComponentZOrder(jLabel1, getContentPane().getComponentCount() - 1);
+        getContentPane().setComponentZOrder(jButton1, 0);
+        getContentPane().setComponentZOrder(jButton2, 0);
+        getContentPane().setComponentZOrder(jButton3, 0);
+        getContentPane().setComponentZOrder(jButton5, 0);
+        getContentPane().setComponentZOrder(jButton6, 0);
+
+        // Wire button actions (the methods exist, but no listeners were attached).
+        jButton1.addActionListener(this::jButton1ActionPerformed);
+        jButton2.addActionListener(this::jButton2ActionPerformed);
+        jButton3.addActionListener(this::jButton3ActionPerformed);
+        jButton5.addActionListener(this::jButton5ActionPerformed);
+        jButton6.addActionListener(this::jButton6ActionPerformed);
+
+        makeButtonInvisible(jButton1);
+        makeButtonInvisible(jButton2);
+        makeButtonInvisible(jButton3);
+        makeButtonInvisible(jButton5);
+        makeButtonInvisible(jButton6);
+
         this.setResizable(false);
+    }
+
+    private static void makeButtonInvisible(javax.swing.JButton button) {
+        if (button == null) {
+            return;
+        }
+        button.setText("");
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setOpaque(false);
     }
 
     @SuppressWarnings("unchecked")
@@ -15,58 +47,27 @@ public class CustomerProfilePanelVisible extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("");
-        jButton1.setPreferredSize(new java.awt.Dimension(80, 60));
-        jButton1.setContentAreaFilled(false);
-        jButton1.setBorderPainted(false);
-        jButton1.setFocusPainted(false);
-        jButton1.setOpaque(false);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        jButton1.setText("jButton1");
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 570, -1, 60));
 
-        jButton2.setText("");
-        jButton2.setPreferredSize(new java.awt.Dimension(80, 60));
-        jButton2.setContentAreaFilled(false);
-        jButton2.setBorderPainted(false);
-        jButton2.setFocusPainted(false);
-        jButton2.setOpaque(false);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
+        jButton2.setText("jButton2");
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 570, -1, 60));
 
-        jButton3.setText("");
-        jButton3.setPreferredSize(new java.awt.Dimension(80, 60));
-        jButton3.setContentAreaFilled(false);
-        jButton3.setBorderPainted(false);
-        jButton3.setFocusPainted(false);
-        jButton3.setOpaque(false);
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
+        jButton3.setText("jButton3");
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 570, -1, 60));
 
-        jButton4.setText("");
-        jButton4.setPreferredSize(new java.awt.Dimension(80, 60));
-        jButton4.setContentAreaFilled(false);
-        jButton4.setBorderPainted(false);
-        jButton4.setFocusPainted(false);
-        jButton4.setOpaque(false);
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 570, -1, 60));
+        jButton5.setText("jButton5");
+        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 400, 120, 130));
+
+        jButton6.setText("jButton6");
+        getContentPane().add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 400, 130, 130));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/barkbites/CustomerDesign/CustomerProfilePanelVisible.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -88,7 +89,14 @@ public class CustomerProfilePanelVisible extends javax.swing.JFrame {
         new CustomerCartPanel().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
-
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new CustomerQrScannerPanel().setVisible(true);
+        this.dispose();
+    } 
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        new CustomerQrScanPanel().setVisible(true);
+        this.dispose();
+    }  
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -101,7 +109,8 @@ public class CustomerProfilePanelVisible extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/com/mycompany/barkbites/CustomerForms/CustomerQrScannerPanel.java
+++ src/main/java/com/mycompany/barkbites/CustomerForms/CustomerQrScannerPanel.java
@@ -5,33 +5,113 @@ public class CustomerQrScannerPanel extends javax.swing.JFrame {
     public CustomerQrScannerPanel() {
         initComponents();
 
+        // Keep the background image behind the click targets.
+        getContentPane().setComponentZOrder(jLabel1, getContentPane().getComponentCount() - 1);
+        getContentPane().setComponentZOrder(jButton1, 0);
+        getContentPane().setComponentZOrder(jButton2, 0);
+        getContentPane().setComponentZOrder(jButton3, 0);
+        getContentPane().setComponentZOrder(jButton4, 0);
+        getContentPane().setComponentZOrder(jButton5, 0);
+
+        // Wire button navigation.
+        jButton1.addActionListener(this::jButton1ActionPerformed);
+        jButton2.addActionListener(this::jButton2ActionPerformed);
+        jButton3.addActionListener(this::jButton3ActionPerformed);
+        jButton4.addActionListener(this::jButton4ActionPerformed);
+        jButton5.addActionListener(this::jButton5ActionPerformed);
+
+        // Make buttons invisible but still clickable.
+        makeButtonInvisible(jButton1);
+        makeButtonInvisible(jButton2);
+        makeButtonInvisible(jButton3);
+        makeButtonInvisible(jButton4);
+        makeButtonInvisible(jButton5);
+
         this.setResizable(false);
+    }
+
+    private static void makeButtonInvisible(javax.swing.JButton button) {
+        if (button == null) {
+            return;
+        }
+
+        java.awt.Dimension size = button.getSize();
+        if (size == null || size.width <= 0 || size.height <= 0) {
+            size = button.getPreferredSize();
+        }
+        if (size != null && size.width > 0 && size.height > 0) {
+            button.setPreferredSize(size);
+            button.setMinimumSize(size);
+            button.setMaximumSize(size);
+            button.setSize(size);
+            button.setBounds(button.getX(), button.getY(), size.width, size.height);
+        }
+
+        button.setEnabled(true);
+        button.setVisible(true);
+        button.setText("");
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setOpaque(false);
+    }
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        new CustomerProfilePanelVisible().setVisible(true);
+        this.dispose();
+    }
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
+        new CustomerHomePagePanel().setVisible(true);
+        this.dispose();
+    }
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
+        new CustomerMenuPanel().setVisible(true);
+        this.dispose();
+    }
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {
+        new CustomerCartPanel().setVisible(true);
+        this.dispose();
+    }
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {
+        new CustomerProfilePanelVisible().setVisible(true);
+        this.dispose();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setText("jButton1");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, -1, 50));
+
+        jButton2.setText("jButton2");
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 570, -1, 60));
+
+        jButton3.setText("jButton3");
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 570, -1, 60));
+
+        jButton4.setText("jButton4");
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 570, -1, 60));
+
+        jButton5.setText("jButton5");
+        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 570, -1, 60));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/barkbites/CustomerDesign/CustomerQrScannerPanel.png"))); // NOI18N
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 650, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -45,6 +125,11 @@ public class CustomerQrScannerPanel extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
