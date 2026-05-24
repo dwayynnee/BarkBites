35eaece02c9919775b3ab2d69022bf418893b5b0
cwossant <dwayynnee@gmail.com>
2026-05-24 16:40:12 +0800

    Refactor Customer Forms and Add New Forms
    - Removed unused buttons from CustomerShowFoodPanel1, CustomerShowFoodPanel2, CustomerShowFoodPanel3, CustomerShowFoodPanel4, and CustomerVoucher classes to streamline the UI.
    - Updated layout constraints for jLabel1 in CustomerShowFoodPanel1, CustomerShowFoodPanel2, CustomerShowFoodPanel3, CustomerShowFoodPanel4, and CustomerVoucher to improve visual alignment.
    - Added new form files for CustomerCheckPhone, CustomerLoginPasswordRecovery, CustomerOrderConfirmed, CustomerOrderProcessing, CustomerReadyPickup, CustomerResetPass, CustomerShowFoodPanel1, CustomerShowFoodPanel2, CustomerShowFoodPanel3, CustomerShowFoodPanel4, and CustomerVoucher to enhance application functionality.
    - Ensured all new forms include appropriate properties and layout settings for consistent behavior across the application.

--- src/main/java/com/mycompany/barkbites/CustomerForms/CustomerReadyPickup.java
+++ src/main/java/com/mycompany/barkbites/CustomerForms/CustomerReadyPickup.java
@@ -9,7 +9,9 @@ public class CustomerReadyPickup extends javax.swing.JFrame {
         this.setResizable(false);
     }
 
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -18,36 +20,38 @@ public class CustomerReadyPickup extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jButton1.setText("jButton1");
         jButton1.setBorderPainted(false);
         jButton1.setContentAreaFilled(false);
         jButton1.setFocusPainted(false);
-        jButton1.addActionListener(evt -> FormNavigator.redirect(this, new CustomerHomePagePanel()));
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 570, 80, 60));
 
+        jButton2.setText("jButton2");
         jButton2.setBorderPainted(false);
         jButton2.setContentAreaFilled(false);
         jButton2.setFocusPainted(false);
-        jButton2.addActionListener(evt -> FormNavigator.redirect(this, new CustomerMenuPanel()));
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 570, 80, 60));
 
+        jButton3.setText("jButton3");
         jButton3.setBorderPainted(false);
         jButton3.setContentAreaFilled(false);
         jButton3.setFocusPainted(false);
-        jButton3.addActionListener(evt -> FormNavigator.redirect(this, new CustomerProfilePanelVisible()));
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 570, 80, 60));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/barkbites/CustomerDesign/CustomerReadyPickup.png")));
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/barkbites/CustomerDesign/CustomerReadyPickup.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -50, 360, 750));
 
         pack();
-    }
+    }// </editor-fold>//GEN-END:initComponents
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(() -> new CustomerReadyPickup().setVisible(true));
     }
 
+    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
 }
--- src/main/java/com/mycompany/barkbites/CustomerForms/CustomerShowFoodPanel4.java
+++ src/main/java/com/mycompany/barkbites/CustomerForms/CustomerShowFoodPanel4.java
@@ -9,45 +9,25 @@ public class CustomerShowFoodPanel4 extends javax.swing.JFrame {
         this.setResizable(false);
     }
 
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setBorderPainted(false);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setFocusPainted(false);
-        jButton1.addActionListener(evt -> FormNavigator.redirect(this, new CustomerHomePagePanel()));
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 570, 80, 60));
-
-        jButton2.setBorderPainted(false);
-        jButton2.setContentAreaFilled(false);
-        jButton2.setFocusPainted(false);
-        jButton2.addActionListener(evt -> FormNavigator.redirect(this, new CustomerMenuPanel()));
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 570, 80, 60));
-
-        jButton3.setBorderPainted(false);
-        jButton3.setContentAreaFilled(false);
-        jButton3.setFocusPainted(false);
-        jButton3.addActionListener(evt -> FormNavigator.redirect(this, new CustomerProfilePanelVisible()));
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 570, 80, 60));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/barkbites/CustomerDesign/CustomerShowFoodPanel4.png")));
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/mycompany/barkbites/CustomerDesign/CustomerShowFoodPanel4.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -50, 360, 750));
 
         pack();
-    }
+    }// </editor-fold>//GEN-END:initComponents
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(() -> new CustomerShowFoodPanel4().setVisible(true));
     }
 
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
 }
